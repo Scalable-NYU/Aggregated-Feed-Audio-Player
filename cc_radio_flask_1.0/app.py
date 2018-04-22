5c5805f6d5b5b5e8b14f608b7cf91944a413dd68
Haoran <mhrcmr@163.com>
2018-04-22 14:03:11 -0400

    Add a simple audio player

--- cc_radio_flask_1.0/app.py
+++ cc_radio_flask_1.0/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, Response, render_template, redirect, url_for
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 import get_my_tweets as tweets
 
@@ -10,13 +10,27 @@ blueprint = make_twitter_blueprint(
 )
 app.register_blueprint(blueprint, url_prefix="/login")
 
+# def streamwav():
+#     def generate():
+#         with open("cc_audio_example/test.mp3", "rb") as fwav:
+#             data = fwav.read(1024)
+#             while data:
+#                 yield data
+#                 data = fwav.read(1024)
+#     return Response(generate(), mimetype="audio/x-wav")
+
 @app.route("/")
 def index():
     if not twitter.authorized:
         return redirect(url_for("twitter.login"))
     resp = twitter.get("account/settings.json")
     assert resp.ok
-    return tweets.main()
+
+    my_tweets = tweets.main()
+
+    return render_template('index.html', display = my_tweets)
+
+    # return "You are @{screen_name} on GitHub".format(screen_name = resp.json()["screen_name"])
 
 # @app.route("/login")
 # def 
